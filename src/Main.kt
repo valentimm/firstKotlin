b14fd39088e218d1029033a0add6c770f3428d66
Matheus Valentim <meduardo.valentim@gmail.com>
2025-02-11 09:59:27 -0300

    feat: improve Hello World

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,19 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println("Hello World! Welcome to Kotlin!!")
+    println("Entre com seu nome")
+
+    val name = readln()
+
+    println("Hello, $name!")
+
+    println("E qual a sua idade?")
+
+    val age = readln().toInt()
+
+    for (i in 1..age) {
+        val ageText = if (i == age) "A sua idade é $i!" else "$i..."
+
+        println(ageText)
+    }
 }
